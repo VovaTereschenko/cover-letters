8e795be82f738fcc5c9e8ac11b44d6eb4dba1887
VovaTereschenko <t.rimidalv@gmail.com>
2025-09-25 16:18:23 +0200

    initial setup of next.js app

---
+++ app/components/ProgressIndication/ProgressIndication.tsx
@@ -0,0 +1,7 @@
+import styles from "./ProgressIndication.module.css";
+
+const ProgressIndication = () => {
+  return <div className={styles.container}>a</div>;
+};
+
+export { ProgressIndication };
